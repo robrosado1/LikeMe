d696491915dc77a703e61b274ef6f2d6e0e4b295
Robinson Rosado <robrosado1@gmail.com>
2018-11-13 11:43:42 -0500

    Create migration tables for users, posts, comments, and friendships

---
+++ db/migrate/20181113162721_create_comments.rb
@@ -0,0 +1,11 @@
+class CreateComments < ActiveRecord::Migration[5.2]
+  def change
+    create_table :comments do |t|
+      t.text :body, null: false
+      t.integer :commenter_id, null: false
+      t.references :commentable, polymorphic: true
+      t.timestamps
+    end
+    add_index :comments, :commentable
+  end
+end
