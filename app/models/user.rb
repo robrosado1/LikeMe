d7cd3d15a51b97e3a8da50904142316e5f0b6da4
App Academy Student <diego.rodriguez923@gmail.com>
2018-11-13 13:35:07 -0500

    Create and fill out user model

---
+++ app/models/user.rb
@@ -0,0 +1,39 @@
+class User < ApplicationRecord
+  validates :username, :email, :session_token, presence: true, :uniqueness: true
+  validates :password_digest, :name, presence: true
+  validates :password, length: { minimum: 6, allow_nil: true }
+
+  after_initialize :ensure_session_token
+  attr_reader :password
+
+  def self.find_by_credentials(username, password)
+    user = User.find_by(username: username);
+    return nil unless user && user.is_password?(password)
+    user
+  end
+
+  def reset_session_token!
+    self.update_attributes(session_token: self.class.generate_session_token)
+    self.session_token
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  private
+
+  def ensure_session_token
+    self.session_token ||= self.reset_session_token!
+  end
+
+  def generate_session_token
+    SecureRandom.urlsafe_base64(16)
+  end
+
+end
