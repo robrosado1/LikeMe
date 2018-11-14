1fef04f1445228b1ff6411440aca9d3ddf59e6cb
Robinson Rosado <robrosado1@gmail.com>
2018-11-13 19:18:00 -0500

    Bootstrap current user to window

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
 
-  helper_method :current_user, :logged_in, :ensure_logged_in
+  helper_method :current_user, :logged_in?, :ensure_logged_in
 
   def current_user
     return nil unless session[:session_token]
