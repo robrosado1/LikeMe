5954ac351e3f707fa227ab515b875371559e50cd
App Academy Student <diego.rodriguez923@gmail.com>
2018-11-13 19:13:06 -0500

    Render frontend with proper elements

---
+++ frontend/components/greeting/greeting.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+
+
+const Greeting = ({ currentUser, logout }) => {
+  const sessionLinks = () => (
+    <nav className="login-signup">
+      <span><Link to="/login">Login</Link> or <Link to="/signup">Sign up!</Link></span>
+    </nav>
+  );
+  const personalGreeting = () => (
+    <hgroup className="header-group">
+      <h2 className="header-name">Hi, {currentUser.username}!</h2>
+      <button className="header-button" onClick={logout}>Log Out</button>
+    </hgroup>
+  );
+
+  return currentUser ? personalGreeting() : sessionLinks();
+};
+
+
+export default Greeting;
