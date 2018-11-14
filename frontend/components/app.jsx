24ca685da74b095eb1b4242ccef2085282f25157
Robinson <robrosado1@gmail.com>
2018-11-14 15:48:21 -0500

    Create signup form on front page

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -4,6 +4,7 @@ import LoginFormContainer from './session_form/login_form_container';
 import SignupFormContainer from './session_form/signup_form_container';
 // import { Route } from 'react-router-dom';
 import { AuthRoute } from '../util/route_util';
+import HomePageLeft from './session_form/homepage_left';
 
 
 const App = () => (
@@ -17,6 +18,7 @@ const App = () => (
       </div>
         <div className="main-content">
           <div className="main-left">
+            <HomePageLeft />
           </div>
           <div className="main-right">
             <AuthRoute exact path="/" component={SignupFormContainer} />
---
+++ frontend/components/session_form/homepage_left.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const HomePageLeft = () => {
+  return (
+    <div className="homepage-left">
+      <h3>Connect with friends and the world around you on Likeme.</h3>
+      <ul className="info-list">
+        <li><strong>See photos and updates</strong> from friends in News Feed.</li>
+        <li><strong>Share what's new</strong> in your life on your Timeline.</li>
+        <li><strong>Find more</strong> of what you're looking for with Facebook Search.</li>
+      </ul>
+    </div>
+  )
+}
+
+export default HomePageLeft;
