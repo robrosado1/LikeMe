c168a9429d8fb46b006c99a4e0dc63ec78785a2d
App Academy Student <diego.rodriguez923@gmail.com>
2018-11-13 19:13:06 -0500

    Render frontend with proper elements

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   context: __dirname,
-  entry: './frontend/likeme.jsx',
+  entry: './frontend/like_me.jsx',
   output: {
     path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
     filename: 'bundle.js'
