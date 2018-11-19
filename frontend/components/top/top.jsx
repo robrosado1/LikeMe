a75e76fe9312883923565072f9d214e584cd46b6
Robinson Rosado <robrosado1@gmail.com>
2018-11-18 22:27:41 -0500

    setup for About section

--- frontend/components/top/top.jsx
+++ frontend/components/top/top.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import Header from '../header/header';
 import NavbarContainer from '../navbar/navbar_container';
+import { Route } from 'react-router-dom';
 import { ProtectedRoute, AuthRoute } from '../../util/route_util';
 
 const PageTop = () => (
   <>
-    <AuthRoute path="/" component={Header} />
+    <Route exact path="/" component={Header} />
+    <Route exact path="/login" component={Header} />
+    <Route exact path="/signup" component={Header} />
     <ProtectedRoute path="/users" component={NavbarContainer} />
   </>
 );
--- frontend/components/wall/wall.jsx
+++ frontend/components/wall/wall.jsx
@@ -2,10 +2,26 @@
 //about, friends, photos, archive(?), and more(dropdown)
 //leftside - intro box, photos box, friends Box
 //rightside - make post, newsfeed/wall-posts
+import React from 'react';
+import { connect } from 'react-redux'
+import { Route } from 'react-router-dom';
+import { AuthRoute } from '../../util/route_util';
 import Headboard from './headboard/headboard';
+import AboutContainer from './about/about_container';
 
-const Wall = () => (
+const Wall = (props) => {
+return (
   <>
-    <Headboard />
+    <Headboard path={props.path}/>
+    <AboutContainer path={props.path}/>
   </>
-);
+)};
+
+
+const mapStateToProps = (state, { location: { pathname } }) => {
+  return ({
+    path: pathname
+  });
+};
+
+export default connect(mapStateToProps)(Wall);
