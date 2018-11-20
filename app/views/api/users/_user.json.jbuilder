e7695faf2a93b12d326843aded3c7027ee86ffa8
Robinson Rosado <robrosado1@gmail.com>
2018-11-20 11:47:45 -0500

    posts now show up in newsfeed

--- app/views/api/users/_user.json.jbuilder
+++ app/views/api/users/_user.json.jbuilder
@@ -1,2 +1,8 @@
 json.extract! user, :id, :email, :fname, :lname, :birthday, :birthmonth,
 :birthyear, :gender
+
+json.set! 'post_ids' do
+  json.array! user.authored_posts do |post|
+    json.id post.id
+  end
+end
--- frontend/components/logged_in/newsfeed/newsfeed_container.js
+++ frontend/components/logged_in/newsfeed/newsfeed_container.js
@@ -1,14 +1,17 @@
 import { connect } from 'react-redux';
 import NewsFeed from './newsfeed';
+import { fetchPosts, createPost } from '../../../actions/post_actions';
 
-const mapStateToProps = ({entities: { users }, session}, ownProps) => {
+const mapStateToProps = ({entities: { users, posts }, session}, ownProps) => {
   return ({
-    currentUser: users[session.id]
+    currentUser: users[session.id],
+    posts
   })
 };
 
 const mapDispatchToProps = dispatch => ({
-  submitPost: post => dispatch()
+  createPost: post => dispatch(createPost(post)),
+  fetchPosts: () => dispatch(fetchPosts())
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(NewsFeed);
