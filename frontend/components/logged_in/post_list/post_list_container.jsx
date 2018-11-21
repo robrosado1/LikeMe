596f6b157d229e22930264c9756b03b1896d8546
Robinson Rosado <robrosado1@gmail.com>
2018-11-20 23:53:12 -0500

    reworking posts

---
+++ frontend/components/logged_in/post_list/post_list_container.jsx
@@ -0,0 +1,21 @@
+import { connect } from 'react-redux';
+import PostList from './post_list';
+import { fetchPosts, createPost } from '../../../actions/post_actions';
+import { fetchUsers } from '../../../actions/user_actions';
+
+const mapStateToProps = ({entities: { users, posts }, session}, ownProps) => {
+  return ({
+    currentUser: users[session.id],
+    posts,
+    users,
+    path: ownProps.path.location.href.split("#")[1]
+  })
+};
+
+const mapDispatchToProps = dispatch => ({
+  createPost: post => dispatch(createPost(post)),
+  fetchPosts: () => dispatch(fetchPosts()),
+  fetchUsers: () => dispatch(fetchUsers())
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(PostList);
