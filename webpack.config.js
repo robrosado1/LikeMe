d82202ec7352afdf6f6052b1b24d12e314b579e8
Robinson <robrosado1@gmail.com>
2018-12-12 11:03:12 -0500

    production environment refactor

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,22 @@
 const path = require('path');
 
+var prodPlugins = [
+  new webpack.DefinePlugin({
+    'process.env': {
+      'NODE_ENV': JSON.stringify('production')
+    }
+  }),
+  new webpack.optimize.UglifyJsPlugin({
+    compress: {
+      warnings: true
+    }
+  })
+];
+
+plugins = plugins.concat(
+  process.env.NODE_ENV === 'production' ? prodPlugins : devPlugins
+)
+
 module.exports = {
   context: __dirname,
   entry: './frontend/like_me.jsx',
@@ -7,6 +24,7 @@ module.exports = {
     path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
     filename: 'bundle.js'
   },
+  plugins: plugins,
   module: {
     rules: [
       {
